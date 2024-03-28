ec3e8e7b07e1a8f76ee8aae8aef32d2f21a83ca5
Juyeon1109 <wndus2884@naver.com>
2024-03-28 16:07:24 +0900

    [Feat] 꿀벌

--- UnityProject_A_24_01/Assets/scripts/ExCubePlayer.cs
+++ UnityProject_A_24_01/Assets/scripts/ExCubePlayer.cs
@@ -5,23 +5,44 @@ using UnityEngine.UI;
 
 public class ExcubePlayer : MonoBehaviour
 {
-    public Rigidbody m_Rigidbody;
-    public int Force = 100;
-    public float checkTime = 0;
-    public Text m_Text;
+    public Text TextUI = null;          // 텍스트 UI
+    public int Count = 0;               // 마우스 클릭 카운터
+    public float Power = 100;           // 물리 힘 수치
+   
+    public float Point = 0;
+    public float checkTime = 0.0f;
+
+    public Rigidbody m_Rigidbody;       // 오브젝트의 강체
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))        //스페이스를 누를 때 조건
-        {
-            m_Rigidbody.AddForce(transform.up * Force);
-        }
-
         checkTime += Time.deltaTime;
         if(checkTime >= 1.0f)
         {
-            Point
+            Point += 1;
+            checkTime = 0.0f;
+        }
 
+
+        if (Input.GetKeyDown(KeyCode.Space))        //스페이스를 누를 땐
+        {
+            Count += 1;                             //마우스가 클릭되었을때 Count를 1씩 올린다
+            TextUI.text = Count.ToString();         //UI 갱신
+            Power = Random.Range(100, 200);         //100 ~ 200 사이의 값의 힘을 준다.
+            m_Rigidbody.AddForce(transform.up * Power);     //Y축으로 설정한 힘을 준다.
+        }
+
+        TextUI.text = Point.ToString();
+
+      
+    }
+    void OnCollisionEnter(Collision collision)      //충돌이 되었을 때
+    {
+        // Debug.Log(collision.gameObject.name);
+        Point = 0;
+        gameObject.transform.position = Vector3.zero;       //플레이어를 원점으로 이동 시킨다.
+    }
+}
 
 
